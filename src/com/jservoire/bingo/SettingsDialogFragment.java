8e415e4087da149dba4ef1f28511f49326ab7b47
Johann-S <johann.servoire@gmail.com>
2014-02-23 17:07:55 +0100

    Sauvegarde du delais et changement de methode de sauvegarde et de chargement des parametres.

--- src/com/jservoire/bingo/SettingsDialogFragment.java
+++ src/com/jservoire/bingo/SettingsDialogFragment.java
@@ -11,24 +11,23 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
-import android.preference.PreferenceManager;
 
 
 public class SettingsDialogFragment extends DialogFragment
 {
-	private DialogInterface.OnClickListener saveListener = new DialogInterface.OnClickListener() {
-
+	private DialogInterface.OnClickListener saveListener = new DialogInterface.OnClickListener() 
+	{
 		@Override
 		public void onClick(DialogInterface arg0, int arg1) {
-			// TODO Auto-generated method stub
-			
-		}	
-
+			savePreferences();
+		}
 	};
 	
-	private DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
-		
+	private DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() 
+	{	
 		@Override
 		public void onClick(DialogInterface dialog, int which) {
 			// TODO Auto-generated method stub
@@ -40,9 +39,31 @@ public class SettingsDialogFragment extends DialogFragment
 	private CompoundButton.OnCheckedChangeListener eventActMusic = new CompoundButton.OnCheckedChangeListener() {
 		@Override
 		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-			setMusicPreference(isChecked);			
+			musicEnable = isChecked;		
 		}
 	};
+	
+	private SeekBar seekDelayBar;
+	private SeekBar.OnSeekBarChangeListener listenerSeekBar = new SeekBar.OnSeekBarChangeListener() 
+	{	
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser){
+			textDelay.setText(Integer.toString(progress));
+		}
+		
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar){
+			delay = seekBar.getProgress();
+		}
+		
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {}
+	};
+	
+	
+	private TextView textDelay;
+	private int delay;
+	private Boolean musicEnable;
 	
 	public static SettingsDialogFragment newInstance(String title) {
 		return new SettingsDialogFragment();
@@ -54,9 +75,18 @@ public class SettingsDialogFragment extends DialogFragment
 		super.onCreateDialog(savedInstanceState);
 		LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View vDialog = inflater.inflate(R.layout.dialog_settings,null);
+		
 		activMusic = (ToggleButton)vDialog.findViewById(R.id.toggleMusic);
 		activMusic.setOnCheckedChangeListener(eventActMusic);
-		activMusic.setChecked(getMusicPreference());
+		
+		seekDelayBar = (SeekBar)vDialog.findViewById(R.id.seekBarDelay);
+		seekDelayBar.setOnSeekBarChangeListener(listenerSeekBar);	
+		textDelay = (TextView)vDialog.findViewById(R.id.textNbDelay);
+		
+		loadPreferences();
+		activMusic.setChecked(musicEnable);
+		seekDelayBar.setProgress(delay);
+		textDelay.setText(Integer.toString(delay));
 		
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		builder.setView(vDialog);
@@ -67,17 +97,19 @@ public class SettingsDialogFragment extends DialogFragment
 		return (Dialog)builder.create();
 	}
 	
-	private void setMusicPreference(Boolean value)
+	private void loadPreferences()
+	{
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		musicEnable = preferences.getBoolean("activMusic", false);
+		delay = preferences.getInt("delay", 3);
+	}
+	
+	private void savePreferences()
 	{
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 		SharedPreferences.Editor editor = preferences.edit();
-		editor.putBoolean("activMusic", value);
-		editor.commit();		
-	}
-	
-	private Boolean getMusicPreference()
-	{
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		return preferences.getBoolean("activMusic", false);		
+		editor.putBoolean("activMusic", musicEnable);
+		editor.putInt("delay", delay);
+		editor.commit();
 	}
 }
